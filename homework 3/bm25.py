d5cb3f34a77c5996b53244d3c0bb545a2f1da9fc
xljsskk <jli@ccs.neu.edu>
2015-11-05 23:38:10 -0500

    Roughly Done

--- homework 3/bm25.py
+++ homework 3/bm25.py
@@ -10,15 +10,26 @@ class Solution:
         self.queryFile = file2
         self.tokenFile = file3
         self.maxNum = num
-        #  todo
-        #  avdl
 
         # read the inverted index
         with open(self.indexFile) as f:
             fRead = f.read()
             lines = fRead.split("\n")
+
+        #  print '-'*50
+        #  for i in range(len(lines)):
+            #  if ast.literal_eval(lines[0]) == 'portabl':
+                #  break
+        #  print i
+        #  print type(lines)
+        #  print ast.literal_eval(lines[0]) == 'orthogon'
+
+        #  print lines.index('orthogon')
+        #  print lines.index('clutter')
+        #  print lines.index('portabl')
+
         self.index = {}
-        for i in range(0,3203,2):
+        for i in range(0,len(lines) - 1,2):
             self.index[ast.literal_eval(lines[i])] = ast.literal_eval(lines[i + 1])
 
         # read the query
@@ -38,43 +49,47 @@ class Solution:
         self.N = len(self.token)
         self.avdl = sum(len(i) for i in self.token) * 1.0 / self.N
 
-        #  debug
-        #  print self.index
-        #  print self.query
-        #  print len(self.token)
-        #  print self.token[0]
-        #  print type(self.token[0])
-        #  print self.token[-1]
-        #  print self.token[1]
-
 
     def calK(self, dl):
-         return = 1.2 * (0.25 + 0.75 * dl / self.avdl)
+         return  1.2 * (0.25 + 0.75 * dl / self.avdl)
 
     def bm25(self):
         #  deal with each query
-        for query in self.query:
+        for q in range(len(self.query)):
             #  score for each doc for a query
             for i in range(len(self.token)):
                 score = 0
                 dl = len(self.token[i])
-                for term in query:
+                K = self.calK(dl)
+                for term in self.query[q]:
+                    #  print term
+                    #  if term in self.index.keys():
                     ni = len(self.index[term])
                     fi = self.token[i].count(term)
+                    #  else:
+                        #  ni = 0
+                        #  fi = 0
+                    score += self.calScore(ni,fi,K )
+                self.scores[(q+1,i+1)] = score
 
-                    score += self.calScore(len() , )
+        print score
 
     def calScore(self, ni, fi, K):
-        res = (self.N - ni + 0.5) / (ni + 0.5) * \
-                2.2 * fi / (K + fi) * \
-                101 * fi / (100 + fi)
-        return log(res ,2)
+        print ni,fi,K,self.N
+        res = 1.0 * (self.N - ni + 0.5) / (ni + 0.5) * \
+                2.2 * fi / 1.0 * (K + fi) * \
+                101 * fi / 1.0 * (100 + fi)
+        # debug
+        if res == 0:
+            return 0
+        else:
+            return math.log(res ,2)
 
 if __name__ == '__main__':
     start_time =  time.time()
-
     inFile , outFile , tokenFile, maxNum = sys.argv[1] , sys.argv[2], sys.argv[3],sys.argv[4]
     a = Solution(inFile , outFile, tokenFile ,maxNum)
+    a.bm25()
     end_time = time.time()
     print "total running time: %d" %(end_time - start_time)
 
