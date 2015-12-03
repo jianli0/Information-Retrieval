3cd25f69618ae3fb24c800a83dfd4dd18262b13f
xljsskk <jli@ccs.neu.edu>
2015-12-02 20:10:29 -0500

    second commit

--- homework 5/hw4.py
+++ homework 5/hw4.py
@@ -7,10 +7,18 @@ class Solution:
         self.relevanceFile = file2
         self.output = []
         self.relevance = {}
+
         # three queries (each with 100 retrived files)
         self.totalResult = 300
+
+        # deal with querys
         self.queryIDs = [12,13,19]
         self.queryMap = {12:1, 13:2, 19:3}
+
+        # deal with ndcgs
+        self.DcgTable = []
+        self.ideaDcgTable = []
+        self.ndcgTable = []
 
         # read the resultFile from homework3
         with open(self.resultFile) as f:
@@ -20,7 +28,7 @@ class Solution:
             element = lines[i].split()
             self.output.append([element[0], element[3], element[2], element[4], None, None, None, None])
         #  print self.output
-        #  [Rank, Document ID, Document score, Relevance level, Precision, Recall, NDCG]
+        #  [queryID, Rank, Document ID, Document score, Relevance level, Precision, Recall, NDCG]
         #  [['1', '3127', '1', '14.404766'] ]
 
 
@@ -37,21 +45,30 @@ class Solution:
                     else:
                         self.relevance[mapid] = [ast.literal_eval(element[2][5:])]
 
-        print self.relevance
-        # { ('12','1523') : '1' , ...}
+        #  print self.relevance
+        #  {1: [1523, 2080, 2246, 2629, 3127]}
         # reading output from hw3 and relevance document
 
-    def precision(self, doc ,relevantNum):
+    def precision(self, doc, isRelevance, relevantNum):
+        if isRelevance:
+            return (relevantNum + 1)* 1.0 / doc[1]
+        else:
+            return relevantNum * 1.0 / doc[1]
 
-        if doc[3] == 1:
-            return (relevantNum + 1)* 1.0 / 100;
+    def recall(self, doc, isRelevance, relevantNum, relevantTotal):
+        if isRelevance:
+            return (relevantNum + 1)* 1.0 / relevantTotal
+        else:
+            return relevantNum * 1.0 / relevantTotal
 
+    def ndcg(self, doc, isRelevance):
+        if isRelevance:
+            dcg =
+            self.ideaDcgTable.append(self.ideaNdcgTable[-1] + dcg)
+            self.ndcgTable.append()
 
+        return self.ndcgTable[-1]
 
-        pass
-
-    def recall(self,):
-        pass
 
     def p20(self,):
         pass
