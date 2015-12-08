cfe7dca1c1b00d32a80fb57382a285118acfd726
xljsskk <jli@ccs.neu.edu>
2015-12-07 22:48:41 -0500

    finish training

--- homework 6/nbtest.py
+++ homework 6/nbtest.py
@@ -1,21 +1,14 @@
+import sys
+import os
 class Solution:
-    def __init__(self):
-        pass
-
-    def readDocs(self):
-        pass
-
-    def printModel(self)
-
 
 
 #  run with
 #  TODO 1
 #  python nbtest.py modelfile /textcat/test/ predictions.txt
 
-if __name__ == '__main__':
-    model , directory = sys.argv[1], sys.argv[2]
-    a = Solution(model)
-    a.printModel()
 
+if __name__ == '__main__':
+    modelfile , directory = sys.argv[1], sys.argv[2]
+    print model.posModel
 
--- homework 6/nbtrain.py
+++ homework 6/nbtrain.py
@@ -1,44 +1,94 @@
 import sys
-import imp
+import os
 
 class Solution:
-    def __init__(self,modelfile):
+    def __init__(self, dire, modelfile):
         self.model = modelfile
-        self.posDir = 0
-        self.negDir = 0
+        self.directory = dire
+
+        self.posDir = self.directory + 'pos'
+        self.negDir = self.directory + 'neg'
+
         #TODO 2
+        self.posNum = 0
+        self.negNum = 0
+
         self.Ppos = 0
         self.Pneg = 0
 
-        self.posModel = {1:1,2:2}
-        self.negModel = {1:2,2:1}
+        self.posModel = {}
+        self.negModel = {}
 
     def readDocs(self):
+        self.posNum = len(os.listdir(self.posDir))
+        self.negNum = len(os.listdir(self.negDir))
 
-        pass
+        self.Ppos = self.posNum * 1.0 / (self.posNum + self.negNum)
+        self.Pneg = self.negNum * 1.0 / (self.posNum + self.negNum)
+        print"self.pos is %f"%self.Ppos
+        print"self.pos is %f"%self.Ppos
+
+        for file in os.listdir(self.posDir):
+            self.token(self.posDir + '/' + file,self.posModel)
+
+        for file in os.listdir(self.negDir):
+            self.token(self.negDir +'/' + file,self.negModel)
+
+    def token(self, file, model):
+        # TODO now
+        with open(file) as f:
+            lines = f.read().split("\n")
+            for line in lines:
+                for j in line.split():
+                    if j in model:
+                        model[j] += 1
+                    else:
+                        model[j] = 1
+
+    def ignoreLessThan5(self):
+        common = list(set(self.posModel.keys()) & set(self.negModel.keys()))
+        posLeft = list(set(self.posModel.keys()) - set(common))
+        negLeft = list(set(self.negModel.keys()) - set(common))
+
+        for i in common:
+            if (self.posModel[i] + self.negModel[i]) < 5:
+                del self.posModel[i]
+                del self.negModel[i]
+
+        for i in posLeft:
+            if self.posModel[i] < 5:
+                del self.posModel[i]
+
+        for i in negLeft:
+            if self.negModel[i] < 5:
+                del self.negModel[i]
 
     def writeModel(self):
-        self.model
-        #  with open(self.model,'wb') as f:
-            #  for i in self.posModel.items():
-                #  f.write("%r\n"%i)
-            #  for j in self.negModel.items():
-                #  f.write()
+        self.ignoreLessThan5()
+        with open('pos'+ self.model,'wb') as f:
+            for i in self.posModel:
+                f.write(i)
+                f.write('\n')
+                f.write('%r\n'%self.posModel[i])
 
-
-
+        with open('neg'+ self.model,'wb') as f:
+            for j in self.negModel:
+                f.write(j)
+                f.write('\n')
+                f.write('%r\n'%self.negModel[j])
 
 #  run with
 #  TODO 1
-#  python nbtrain.py /textcat/train/  modelfile
-#  python nbtrain.py /textcat/train/ model
+#  python nbtrain.py /textcat/train/ model.txt
 
 if __name__ == '__main__':
     directory , modelfile = sys.argv[1], sys.argv[2]
-    # load the module
-    fp, pathname, description = imp.find_module(modelfile)
-    loadedModel =  imp.load_module(modelfile, fp, pathname, description)
-    print loadedModel.posModel
+    a = Solution(os.getcwd() + directory, modelfile)
+    #  a.writeModel()
+    #  print directory,modelfile
+    # loading the module
+    a.readDocs()
+    a.writeModel()
 
 
 
